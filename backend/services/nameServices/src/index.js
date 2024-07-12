7e5cd353602a638ca69379a42860b20167f1c514
InaneInaccompli <92868301+InaneInaccompli@users.noreply.github.com>
2024-07-12 15:36:38 +0200

    NameService is now usable and can return names !

--- backend/services/nameServices/src/index.js
+++ backend/services/nameServices/src/index.js
@@ -1,1 +1,15 @@
-require('./nameService');
+import express from 'express'
+import dotenv from 'dotenv'
+import { router } from './nameService.js'
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 5005
+
+app.use('/api/v1/name', router);
+
+app.listen(PORT, () => {
+    console.log(`Name Service is running on port ${PORT}`);
+    console.log(`On localhost, you can click on this link : http://localhost:${PORT}/api/v1/name/random`);
+});
