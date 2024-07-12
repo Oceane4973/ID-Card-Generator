fa50c89e1be46e8032b00d8912e3054bb91a0eba
InaneInaccompli <92868301+InaneInaccompli@users.noreply.github.com>
2024-07-12 16:30:59 +0200

    EF 2.1.1 - Ajout d'un second service Name

--- backend/services/nameServices/src/nameService.js
+++ backend/services/nameServices/src/nameService.js
@@ -20,4 +20,30 @@ router.get('/random', async (req, res) => {
     res.send(returnValue);
 });
 
+router.get('/byGender', async (req, res) => {
+    if(!req.query.gender) {
+        return res
+            .status(400)
+            .send({ error: 'Gender query parameter is required' });
+    } else if(req.query.gender != "female" && req.query.gender != "male") {
+        return res
+            .status(400)
+            .send({ error: 'Gender msut be female or male' });
+    }
+    let APIbyGender = apiURL + "?gender=" + req.query.gender;
+    let returnValue = await fetch(APIbyGender)
+    .then(response => {
+        if(!response.ok) throw new Error('API response was not ok');
+        return response.json();
+    })
+    .then(value => {
+        let result = {
+            firstName: value.results[0].name.first,
+            lastName: value.results[0].name.last,
+        }
+        return result;
+    });
+    res.send(returnValue);
+});
+
 export { router };
