482029aec0fd7bdc8eee8b08c8f2a144b5263102
Océane MONGES <oceane@bebou.oceanemonges.com>
2024-07-12 13:03:15 +0200

    Create script for all microservices to fix node deployement

---
+++ backend/services/ageServices/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+NPM_PATH=$(which npm)
+
+if [ -z "$NPM_PATH" ]; then
+  echo "Error: npm is not installed or not in the PATH."
+  exit 1
+fi
+
+if [[ ":$PATH:" != *":$(dirname $NPM_PATH):"* ]]; then
+  export PATH=$PATH:$(dirname $NPM_PATH)
+fi
+
+if [ ! -f "package.json" ]; then
+    echo "Executing npm init -y"
+    npm init -y
+    npm install axios dotenv express
+else
+    echo "npm init -y already executed"
+fi
+
+
+if [ -f "package.json" ]; then
+    if grep -q '"main":' package.json; then
+        sed -i 's#"main": "[^"]*"#"main": "src/index.js"#' package.json
+    else
+        sed -i '/^{/a \  "main": "src/index.js",' package.json
+    fi
+    if grep -q '"start":' package.json; then
+        sed -i 's#"start": "[^"]*"#"start": "node src/index.js"#' package.json
+    else
+        sed -i '/"scripts": {/a \    "start": "node src/index.js",' package.json
+    fi
+fi
+
+echo "Les informations ont été mises à jour dans package.json."
