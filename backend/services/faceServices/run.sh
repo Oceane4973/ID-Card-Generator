3cc3949be16297933a49677462084734e65054f1
Sydnec <>
2024-07-16 17:15:49 +0200

    fix scipts for python setup and run including virtual environnement

--- backend/services/faceServices/run.sh
+++ backend/services/faceServices/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Chemin vers python3
 PYTHON_PATH=$(which python3)
 
 if [ -z "$PYTHON_PATH" ]; then
@@ -7,9 +8,14 @@ if [ -z "$PYTHON_PATH" ]; then
   exit 1
 fi
 
+# Vérifier si le chemin de python3 est dans le PATH
 if [[ ":$PATH:" != *":$(dirname $PYTHON_PATH):"* ]]; then
   export PATH=$PATH:$(dirname $PYTHON_PATH)
 fi
 
+# Activer l'environnement virtuel
+source .venv/bin/activate
+
+# Exécuter l'application
 echo "Running application"
 python3 src/main.py
--- backend/services/faceServices/setup.sh
+++ backend/services/faceServices/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Chemin vers python3
 PYTHON_PATH=$(which python3)
 
 if [ -z "$PYTHON_PATH" ]; then
@@ -7,10 +8,21 @@ if [ -z "$PYTHON_PATH" ]; then
   exit 1
 fi
 
+# Vérifier si le chemin de python3 est dans le PATH
 if [[ ":$PATH:" != *":$(dirname $PYTHON_PATH):"* ]]; then
   export PATH=$PATH:$(dirname $PYTHON_PATH)
 fi
 
+# Créer un environnement virtuel
+if [ ! -d ".venv" ]; then
+  echo "Creating virtual environment"
+  python3 -m venv .venv
+fi
+
+# Activer l'environnement virtuel
+source .venv/bin/activate
+
+# Créer requirements.txt si non existant
 if [ ! -f "requirements.txt" ]; then
     echo "Creating requirements.txt"
     touch requirements.txt
@@ -20,14 +32,17 @@ if [ ! -f "requirements.txt" ]; then
     echo "imageio" >> requirements.txt
 fi
 
+# Installer les dépendances
 echo "Installing dependencies"
 pip install -r requirements.txt
 
+# Créer le répertoire src si non existant
 if [ ! -d "src" ]; then
     echo "Creating src directory"
     mkdir src
 fi
 
+# Créer src/main.py si non existant
 if [ ! -f "src/main.py" ]; then
     echo "Creating src/main.py"
     touch src/main.py
