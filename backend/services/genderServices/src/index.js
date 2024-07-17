675e8a2d164db4474e6823bb3830274bdff57c5e
Sydnec <>
2024-07-17 13:26:55 +0200

    Gender service

--- backend/services/genderServices/src/index.js
+++ backend/services/genderServices/src/index.js
@@ -1,1 +1,16 @@
-require('./genderService');
+import express from 'express';
+import dotenv from 'dotenv';
+import { router } from './genderService.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5004;
+
+app.use('/api/v1/gender', router);
+
+app.listen(port, () => {
+    console.log(`Gender Service is running on port ${port}`);
+    console.log(`On localhost, you can click on this link : http://localhost:${port}/api/v1/gender/byName`);
+    console.log(`On localhost, you can click on this link : http://localhost:${port}/api/v1/gender/byNameAndOrigin`);
+});
