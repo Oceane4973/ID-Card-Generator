e26151b4d465a06f55b6053e7fa927c5026c4c48
InaneInaccompli <92868301+InaneInaccompli@users.noreply.github.com>
2024-07-18 16:40:35 +0200

    Added unit tests form nameService

--- backend/services/nameServices/src/nameService.js
+++ backend/services/nameServices/src/nameService.js
@@ -5,15 +5,22 @@ const apiURL = "https://randomuser.me/api/";
 const router = express.Router();
 
 router.get('/random', async (req, res) => {
-    let returnValue = await fetch(apiURL)
-    .then(response => {
-        if(!response.ok) throw new Error('API response was not ok');
-        return response.json();
-    })
-    .then(value => {
-        return apiResponseConstructor(value.results[0].name.first, value.results[0].name.last);
-    });
-    res.send(returnValue);
+    try {
+        let returnValue = await fetch(apiURL)
+        .then(response => {
+            if(!response.ok) throw new Error('API response was not ok');
+            return response.json();
+        })
+        .then(value => {
+            return apiResponseConstructor(value.results[0].name.first, value.results[0].name.last);
+        });
+        console.log(returnValue);
+        res.send(returnValue);
+    } catch(error) {
+        return res
+            .status(400)
+            .send({ error: error.message });
+    }
 });
 
 router.get('/byGender', async (req, res) => {
@@ -27,23 +34,28 @@ router.get('/byGender', async (req, res) => {
             .send({ error: 'Gender must be female or male' });
     }
     let APIbyGender = apiURL + "?gender=" + req.query.gender;
-    let returnValue = await fetch(APIbyGender)
-    .then(response => {
-        if(!response.ok) throw new Error('API response was not ok');
-        return response.json();
-    })
-    .then(value => {
-        return apiResponseConstructor(value.results[0].name.first, value.results[0].name.last);
-    });
-    res.send(returnValue);
+    try {
+        let returnValue = await fetch(APIbyGender)
+        .then(response => {
+            if(!response.ok) throw new Error('API response was not ok');
+            return response.json();
+        })
+        .then(value => {
+            return apiResponseConstructor(value.results[0].name.first, value.results[0].name.last);
+        });
+        res.send(returnValue);
+    } catch(error) {
+        return res
+            .status(400)
+            .send({ error: error.message });
+    }
 });
 
 const apiResponseConstructor = (firstName, lastName) => {
-    const result = {
+    return {
         firstName: firstName,
         lastName: lastName,
     }
-    return result;
 }
 
-export { router };
+export { router, apiURL };
